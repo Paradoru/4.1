aa51fc090d68422ded99963e69a8c7d10fa30a45
Paradoru <LicLexa@yandex.ru>
2021-11-30 23:33:16 +0200

    Добавьте файлы проекта.

---
+++ 4.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int [] a = new int[15]; 
+                int sum = 0;
+                Random rnd = new Random();
+                string s = null; 
+
+                for (int i = 0; i < 15; i++)
+                {
+                    a [i] = rnd.Next(1, 100); 
+                    s = s + a [i].ToString() + " "; 
+                    sum = sum + a [i]; 
+                }
+                Console.WriteLine("Начальный массив: " + s); 
+                Console.WriteLine("Среднее арифметическое: " + (sum / 15));
+                s = null;
+                for (int i = 0; i < 15; i++)
+                {
+                    a [i] = a [i] - (sum / 15);
+                    s = s + a [i].ToString() + " ";
+                }
+                Console.WriteLine("Новый массив = " + s);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
